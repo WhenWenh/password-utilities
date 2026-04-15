fcd2063f649c88a764e7ee9d2e252d0b6f410fef
Wenhao <if24b216@technikum-wien.at>
2026-04-15 15:36:10 +0200

    test: added test for password security fix

--- test/index.test.js
+++ test/index.test.js
@@ -9,3 +9,19 @@ test("should return true for a strong password", () => {
     const password = generateStrongPassword(16);
     expect(isStrongPassword(password)).toBe(true);
 });
+
+test("should retry password generation until the password is strong", () => {
+    const randomSpy = jest.spyOn(Math, 'random');
+    const indexToRandomValue = (index) => index / 74;
+
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 27, 28, 29, 30, 31, 53, 62]
+        .forEach((index) => randomSpy.mockReturnValueOnce(indexToRandomValue(index)));
+
+    const password = generateStrongPassword(8);
+
+    expect(password).toBe("Abcdef1!");
+    expect(isStrongPassword(password)).toBe(true);
+    expect(randomSpy).toHaveBeenCalledTimes(16);
+
+    randomSpy.mockRestore();
+});
